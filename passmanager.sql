418ecb2ac30cb2d11319e45199370619b74f5dbe
Ansh sharma <ansh11639@gmail.com>
2024-06-09 18:06:07 +0530

    Add files via upload

---
+++ passmanager.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE password_users;
+USE password_users;
+
+CREATE TABLE log_users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    contact_no VARCHAR(15),
+    password VARCHAR(100) NOT NULL
+);
+
+
+
+CREATE TABLE web_passwords (
+id INT AUTO_INCREMENT PRIMARY KEY,
+user_id INT,
+website VARCHAR(70) NOT NULL,
+username VARCHAR(70) NOT NULL,
+password VARCHAR(70) NOT NULL,
+FOREIGN KEY (user_id) REFERENCES log_users(id) ON DELETE CASCADE
+);
+
+DROP TABLE web_passwords;
+DROP TABLE log_users;
+
+CREATE USER 'Gutsss'@'localhost' IDENTIFIED BY 'gutsss77';
+GRANT ALL PRIVILEGES ON password_users.* TO 'Gutsss'@'localhost';
+FLUSH PRIVILEGES;
+
+SELECT * FROM web_passwords;
+SELECT * FROM log_users;
+
+DROP USER 'Gutsss'@'localhost';
+FLUSH PRIVILEGES;
